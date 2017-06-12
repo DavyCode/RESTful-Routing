6c558c2a5ca053e87f0ee46ecb2781c9bd935879
Davy'd <pdaswift3009@gmail.com>
2017-06-12 18:03:01 +0100

    initial commit with file

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# RESTful-Routing
+## Blog app
+* RESTful blog app using Express and Node 
+* cd into blog folder && run npm install to install all dependencies
+* npm start or node app.js to run app
+* goto localhost:3000 on your favourite browser to view blog 
---
+++ app.js
@@ -0,0 +1,108 @@
+var express = require("express"),
+    bodyParser = require("body-parser"),
+    methodOverride = require('method-override'),
+    mongoose = require("mongoose"),
+    app = express();
+
+
+mongoose.connect("mongodb://127.0.0.1:27017/Blog_app", (err) => {
+    (err) ? console.error(err, 'Error Connecting to Database!'):
+        console.log('Database Connected Successfully. Build Safely!')
+});
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(__dirname + "/public"));
+app.use(methodOverride("_method"));
+
+//default view engines
+app.set("view engine", "ejs");
+
+
+
+// MONGOOSE/MODEL SCHEMA
+var blogSchema = new mongoose.Schema({
+    title: String,
+    image: String,
+    body: String,
+    created: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+var Blog = mongoose.model("Blog", blogSchema);
+
+// Blog.create({
+//     title: "Tester Blog",
+//     image: '/images/1.jpg',
+//     body: "Hello just testing out this blog!!!"
+// })
+
+
+
+
+
+//RESTFUL ROUTES
+app.get("/", (req, res) => {
+    res.redirect("/blogs");
+});
+
+//index Route
+app.get("/blogs", (req, res) => {
+    Blog.find({}, (err, blogs) => {
+        (err) ? console.log(err): res.render("index", { blogs: blogs })
+    })
+});
+
+// NEW ROUTE
+app.get('/blogs/new', (req, res) => {
+    res.render("new")
+});
+
+// CREATE ROUTE
+app.post('/blogs', (req, res) => {
+    //create new blog
+    Blog.create(req.body.blog, (err, newBlog) => {
+        if (err) {
+            res.render('new')
+        }
+        res.redirect('/blogs')
+    });
+});
+
+// SHOW ROUTE
+app.get('/blogs/:id', (req, res) => {
+    Blog.findById(req.params.id, (err, foundBlog) => {
+        (err) ? res.redirect('/blogs'): res.render('show', { blog: foundBlog });
+
+    })
+})
+
+// EDIT ROUTE
+app.get('/blogs/:id/edit', (req, res) => {
+    Blog.findById(req.params.id, (err, foundBlog) => {
+        (err) ? res.redirect('/blogs'): res.render('edit', { blog: foundBlog });
+    });
+});
+
+// UPDATE ROUTE
+app.put('/blogs/:id', (req, res) => {
+    Blog.findByIdAndUpdate(req.params.id, req.body.blog, (err, updatedBlog) => {
+        (err) ? res.redirect('/blogs'): res.redirect('/blogs/' + req.params.id);
+    });
+})
+
+
+// DESTROY ROUTE
+app.delete('/blogs/:id', (req, res) => {
+    Blog.findByIdAndRemove(req.params.id, (err) => {
+        (err) ? res.redirect('/blogs'): res.redirect('/blogs');
+    });
+})
+
+
+//Setup the port for the server to listen on ==> port 3000
+const PORT = process.env.PORT || 3000;
+
+// Start the server, listening on port = PORT
+app.listen(PORT);
